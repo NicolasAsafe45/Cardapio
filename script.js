b2f77ad28c5abd93e8300cd9ff35b0ac88493e9e
NicolasAsafe45 <nicolasasafe45@gmail.com>
2024-07-28 15:50:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const navlist = document.querySelector(".listNav")
+
+function hide(){
+    navlist.classList.toggle("hide")
+}
+
+//////////////////////////////////////////////////////////
+const CarrinhoRun = () =>{
+    const container = document.querySelector(".cardapio")
+    container.style.display = "none"
+    
+}
